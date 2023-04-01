c28e4af61eaadf976fbf4c024f5258c97a8c5ab4
Aitor Gómez <spark2k06@gmail.com>
2023-04-01 09:29:18 +0200

    Cleanup project files.

--- HW/common/dcm.v
+++ HW/common/dcm.v
@@ -66,6 +66,7 @@
 // __primary__________50.000____________0.010
 
 `timescale 1ps/1ps
+`default_nettype wire
 
 (* CORE_GENERATION_INFO = "dcm,clk_wiz_v3_6,{component_name=dcm,use_phase_alignment=true,use_min_o_jitter=false,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,feedback_source=FDBK_AUTO,primtype_sel=PLL_BASE,num_out_clk=4,clkin1_period=20.0,clkin2_period=20.0,use_power_down=false,use_reset=false,use_locked=false,use_inclk_stopped=false,use_status=false,use_freeze=false,use_clk_valid=false,feedback_type=SINGLE,clock_mgr_type=AUTO,manual_override=false}" *)
 module dcm
